eedba0aadfed71bb0ad5d7d61f19603207f97d6c
Izma Raifani <izurai@gmail.com>
2022-04-20 12:13:51 +0700

    tambah project danasyariah

--- src/projectlist.js
+++ src/projectlist.js
@@ -1,6 +1,7 @@
 import dailyrecipe from './images/dailyrecipe.png'
 import rockpaperscissor from './images/rockpaperscissor.png'
 import restcountriesapi from './images/restcountriesapi.png'
+import danasyariah from './images/DanaSyariah.png'
 
 const projectlist = [
   {   id: 'project1', 
@@ -9,6 +10,13 @@ const projectlist = [
       preview: dailyrecipe,
       techUsed:'reactjs, express, nodejs, axios, mongoDb Atlas, firebase',
       link:'https://dailyrecipeapp.netlify.app/' 
+  },
+  {   id: 'project4', 
+      name: 'Landing Page Dana Syariah',
+      description: 'Landing page untuk lembaga keuangan syariah di depok',
+      preview: danasyariah,
+      techUsed:'html, css, javascript',
+      link:'https://dana-syariah.netlify.app/' 
   },
   {   id: 'project2', 
       name: 'Rock Paper Scissor',
@@ -24,6 +32,7 @@ const projectlist = [
       techUsed:'html, sass, javascript',
       link:'https://izurai27.github.io/rest-countries-api-with-color-theme-switcher-master/' 
   },
+  
   
 ]
 
