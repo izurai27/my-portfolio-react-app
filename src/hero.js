b1e7e9f2748cfcff7f33259aeed0ee63c7c1304f
Izma Raifani <izurai@gmail.com>
2022-05-13 14:56:06 +0700

    scroll animation test

--- src/hero.js
+++ src/hero.js
@@ -5,31 +5,35 @@ import femaleProgrammer from './images/female-programmer.svg'
 import logo from './images/iR-logos_transparent.png'
 
 function Hero(){
-    
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal-hero");
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
+          }
+        }
+      }
+
+      window.addEventListener("scroll", reveal);
+
+    // To check the scroll position on page load
+    reveal();
 
     return(
         <div className="hero">
-            {/* <div className='wrapper'>
-            <img className='heroimgprofile' src={izma} alt='profilepic'></img>
-                <h1 className='title'>izma raifani</h1>
-                <p className='subtitle'>a frontend developer<br/> and <br/>product manager</p>
+            <div className="hero-left reveal-hero">
+                <div className="name"><img className='logo' src={logo} alt="logo" />Izma Raifani </div>
+                <p className="my-title">a web developer</p>
+                <a href="#project"><button>see my works</button></a>
             </div>
-             */}
-            
-            
-                 <div className="hero-left">
-                 {/* <img className='logo' src={logo} alt="logo" /> */}
-                    <div className="name"><img className='logo' src={logo} alt="logo" />Izma Raifani </div>
-                    <p className="my-title">a web developer</p>
-                      <a href="#project"><button>see my works</button></a>
-                    
-                 </div>
-                 <div className='hero-right'>
-                    <img src={femaleProgrammer} alt="female-programmer-ilustration" />   
-                    {/* <span className='freepik'>Designed by <a href='https://www.freepik.com/free-vector/css-html-programming-languages-computer-programming-coding-it-female-programmer-cartoon-character-software-website-development-vector-isolated-concept-metaphor-illustration_12083314.htm#query=web%20developer&position=22&from_view=search'>Freepik</a></span> */}
-                 </div>
-             
-
+            <div className='hero-right reveal-hero'>
+                <img src={femaleProgrammer} alt="female-programmer-ilustration" />   
+            </div>
         </div>
         
     )
